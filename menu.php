a23fd7ab5337512b6ffeebdbb7d6d56dec809d80
Fabio Junior B. Barbosa <fabio.barbosa@USEALL.CORP>
2020-10-14 14:19:40 -0300

    Create div collapsible in menu

--- menu.php
+++ menu.php
@@ -67,9 +67,16 @@
         content: "\2212";
       }
 
-      .content {
+      /* .content {
         padding: 0 18px;
         max-height: 0;
+        overflow: hidden;
+        transition: max-height 0.2s ease-out;
+        background-color: #f1f1f1;
+      } */
+      .content {
+        padding: 0 18px;
+        max-height: 100px;
         overflow: hidden;
         transition: max-height 0.2s ease-out;
         background-color: #f1f1f1;
@@ -104,6 +111,22 @@
     </div>
     <!-- <div class="container">  -->
     </div>
+     <!-- <script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  </script> -->
 </body>
 
 </html>
